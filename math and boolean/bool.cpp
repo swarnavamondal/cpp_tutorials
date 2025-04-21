7ba11cb64fef7775097c425baf9c1ed17442cb54
swarnava <swarnavamondal2005@gmail.com>
2025-04-21 17:49:22 +0530

    21/04/25

---
+++ math and boolean/bool.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    bool a = true;
+    bool b = false;
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+    int x = 4;
+    int y = 5;
+    cout << "x == y: " << (x == y) << endl; // false
+    cout << "x != y: " << (x != y) << endl; // true
+}
---
+++ math and boolean/math.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    cout<< min(9,99)<<endl;
+    cout<< max(545,435)<<endl;
+    cout<< abs(-9)<<endl;
+    cout<< sqrt(16)<<endl;
+    cout<< round(4.5)<<endl;
+    cout<< log(10)<<endl;  //default base e
+    cout<< log10(100)<<endl; //base 10
+    cout<< pow(2,3)<<endl; 
+    cout<< sin(90)<<endl; 
+    cout<< cos(90)<<endl; 
+    
+}
